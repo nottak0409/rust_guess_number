b9ca6cc2a26234cb8d2dc0ccd5a906bcf14134a1
nottak0409 <nottak0409@gmail.com>
2022-05-06 13:43:29 +0900

    [add]数当てゲームの本文作成

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("数を当ててみましょう");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
     println!("予想を入力してください");
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("行の読み込みに失敗しました");
+
+    let guess: u32 = guess.trim().parse()
+                    .expect("数値を入力してください");
     println!("次のように予想しました {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("正解よりも小さいです"),
+        Ordering::Greater => println!("正解よりも大きいです"),
+        Ordering::Equal => println!("正解です"),
+    }
 }
