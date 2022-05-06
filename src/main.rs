75526721bb86fc24ab77ce89425be1d045d80edc
nottak0409 <nottak0409@gmail.com>
2022-05-06 13:51:01 +0900

    [add]数当てゲームの調整

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,24 @@ fn main() {
     println!("数を当ててみましょう");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
-    println!("予想を入力してください");
-    let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("行の読み込みに失敗しました");
+    loop {
+        println!("予想を入力してください");
+        let mut guess = String::new();
 
-    let guess: u32 = guess.trim().parse()
-                    .expect("数値を入力してください");
-    println!("次のように予想しました {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("正解よりも小さいです"),
-        Ordering::Greater => println!("正解よりも大きいです"),
-        Ordering::Equal => println!("正解です"),
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("行の読み込みに失敗しました");
+        println!("次のように予想しました {}", guess);
+        let guess: u32 = guess.trim().parse()
+                        .expect("数値を入力してください");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("正解よりも小さいです"),
+            Ordering::Greater => println!("正解よりも大きいです"),
+            Ordering::Equal => {
+                println!("正解です");
+                break;
+            }
+        }
     }
 }
